5ec74706f8b0cfd837b039bb24dec325b98e64ac
Janelle Archer <jarcher@patriotsoftware.com>
2021-06-18 10:54:44 -0400

    Initial commit

---
+++ action.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+
+export HELM_EXPERIMENTAL_OCI=1
+helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" -n "${INPUT_NAMESPACE}" | grep "app.kubernetes.io/name"
+#helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
+echo "Deploying using values file for: ${INPUT_RELEASE_NAME}"
+echo "Best steps to investigate if step fails:"
+echo "- Run your branch in a container locally"
+echo "- Run the deployment again, but check the kube-dashboard while it's deploying -- especially the replicaset for errors, and the pod logs for errors"
+helm upgrade --install "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}"
+
+
+helm status "${INPUT_RELEASE_NAME}"
+echo "✅ Helm upgrade complete"
