76ef65b13b41177a4dbbe2af2b3bdf446b028478
Janelle Archer <jarcher@patriotsoftware.com>
2021-06-25 15:18:43 -0400

    Testing

--- action.sh
+++ action.sh
@@ -14,16 +14,16 @@ echo ${INPUT_VALUES_FILE}
 
 #echo ${INPUT_VALUES_FILE} | sed 's/,//g'
 #echo ${INPUT_VALUES_FILE}
-helm template ${values-string} --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
+# helm template ${values-string} --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
 
-#helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
-echo "Deploying using values file for: ${INPUT_RELEASE_NAME}"
-echo "Best steps to investigate if step fails:"
-echo "- Run your branch in a container locally"
-echo "- Run the deployment again, but check the kube-dashboard while it's deploying -- especially the replicaset for errors, and the pod logs for errors"
+# #helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
+# echo "Deploying using values file for: ${INPUT_RELEASE_NAME}"
+# echo "Best steps to investigate if step fails:"
+# echo "- Run your branch in a container locally"
+# echo "- Run the deployment again, but check the kube-dashboard while it's deploying -- especially the replicaset for errors, and the pod logs for errors"
 
-#helm upgrade --install "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}"
-helm upgrade --install ${values-string} --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
+# #helm upgrade --install "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}"
+# helm upgrade --install ${values-string} --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
 
-helm status "${INPUT_RELEASE_NAME}"
-echo "✅ Helm upgrade complete"
+# helm status "${INPUT_RELEASE_NAME}"
+# echo "✅ Helm upgrade complete"
