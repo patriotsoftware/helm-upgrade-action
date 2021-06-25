524046143a99b49ede024fe82a936e05607f7c43
Janelle Archer <jarcher@patriotsoftware.com>
2021-06-25 15:23:42 -0400

    Testing

--- action.sh
+++ action.sh
@@ -9,8 +9,7 @@ export HELM_EXPERIMENTAL_OCI=1
 INPUT_VALUES_FILE="-f "${INPUT_VALUES_FILE}
 echo ${INPUT_VALUES_FILE}
 #suffix=" -f ";  echo ${INPUT_VALUES_FILE} | sed -E "s/([^,]+)/\1${suffix}/g"
-INPUT_VALUES_FILE="-f "${INPUT_VALUES_FILE}
-INPUT_VALUES_FILE=echo ${INPUT_VALUES_FILE} | sed -r 's/[,]+/ -f /g'
+INPUT_VALUES_FILE=$(echo ${INPUT_VALUES_FILE} | sed -r 's/[,]+/ -f /g')
 echo ${INPUT_VALUES_FILE}
 
 #echo ${INPUT_VALUES_FILE} | sed 's/,//g'
