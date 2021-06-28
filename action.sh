bd1f13dca8a70ee851bfc6a4687ab3a24b134776
Janelle Archer <jarcher@patriotsoftware.com>
2021-06-28 10:05:01 -0400

    Testing out Linux eval cmd.

--- action.sh
+++ action.sh
@@ -7,15 +7,11 @@ export HELM_EXPERIMENTAL_OCI=1
 
 
 INPUT_VALUES_FILE="-f "${INPUT_VALUES_FILE}
-echo ${INPUT_VALUES_FILE}
-#suffix=" -f ";  echo ${INPUT_VALUES_FILE} | sed -E "s/([^,]+)/\1${suffix}/g"
 INPUT_VALUES_FILE=$(echo ${INPUT_VALUES_FILE} | sed -r 's/[,]+/ -f /g')
 echo ${INPUT_VALUES_FILE}
 
-
-echo ${INPUT_VALUES_FILE}
 helm-template-cmd="helm template ${INPUT_RELEASE_NAME} ${INPUT_BASE_CHART} ${INPUT_VALUES_FILE} --set ${INPUT_ADDITIONAL_VALUES} -n ${INPUT_NAMESPACE}"
-echo ${helm-template-cmd}
+echo $helm-template-cmd
 eval $helm-template-cmd
 #helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" "${INPUT_VALUES_FILE}" --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
 
@@ -26,7 +22,7 @@ echo "- Run your branch in a container locally"
 echo "- Run the deployment again, but check the kube-dashboard while it's deploying -- especially the replicaset for errors, and the pod logs for errors"
 
 #helm upgrade --install "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}"
-helm upgrade --install ${INPUT_VALUES_FILE} --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
+#helm upgrade --install ${INPUT_VALUES_FILE} --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
 
 helm status "${INPUT_RELEASE_NAME}"
 echo "✅ Helm upgrade complete"
