a3485efcb3ca5c0cae0414ab7791993c6b35c1e0
Janelle Archer <jarcher@patriotsoftware.com>
2021-06-28 10:01:40 -0400

    Testing eval command

--- action.sh
+++ action.sh
@@ -14,14 +14,19 @@ echo ${INPUT_VALUES_FILE}
 
 
 echo ${INPUT_VALUES_FILE}
-helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"  -f ./values.yaml -f ./values-2.yaml --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
+helm-template-cmd="helm template ${INPUT_RELEASE_NAME} ${INPUT_BASE_CHART} ${INPUT_VALUES_FILE} --set ${INPUT_ADDITIONAL_VALUES} -n ${INPUT_NAMESPACE}"
+echo ${helm-template-cmd}
+eval $helm-template-cmd
+#helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" "${INPUT_VALUES_FILE}" --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
+
+
 echo "Deploying using values file for: ${INPUT_RELEASE_NAME}"
 echo "Best steps to investigate if step fails:"
 echo "- Run your branch in a container locally"
 echo "- Run the deployment again, but check the kube-dashboard while it's deploying -- especially the replicaset for errors, and the pod logs for errors"
 
 #helm upgrade --install "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}"
-helm upgrade --install -f ./values.yaml -f ./values-2.yaml --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
+helm upgrade --install ${INPUT_VALUES_FILE} --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
 
 helm status "${INPUT_RELEASE_NAME}"
 echo "✅ Helm upgrade complete"
