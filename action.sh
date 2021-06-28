0cb59717976d120e8e0c015c902e2de6d25cc2c7
Janelle Archer <jarcher@patriotsoftware.com>
2021-06-28 14:02:51 -0400

    Testing out Linux eval cmd.

--- action.sh
+++ action.sh
@@ -11,9 +11,7 @@ INPUT_VALUES_FILE=$(echo ${INPUT_VALUES_FILE} | sed -r 's/[,]+/ -f /g')
 echo ${INPUT_VALUES_FILE}
 
 helm_template_cmd="helm template ${INPUT_RELEASE_NAME} ${INPUT_BASE_CHART} ${INPUT_INLINE_VALUES} ${INPUT_VALUES_FILE} --set ${INPUT_ADDITIONAL_VALUES} -n ${INPUT_NAMESPACE}"
-echo $helm_template_cmd
 eval $helm_template_cmd
-#helm template "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" "${INPUT_VALUES_FILE}" --set "${INPUT_ADDITIONAL_VALUES}" -n "${INPUT_NAMESPACE}"
 
 
 echo "Deploying using values file for: ${INPUT_RELEASE_NAME}"
@@ -23,8 +21,6 @@ echo "- Run the deployment again, but check the kube-dashboard while it's deploy
 
 helm_upgrade_cmd="helm upgrade --install ${INPUT_INLINE_VALUES} ${INPUT_VALUES_FILE} --set ${INPUT_ADDITIONAL_VALUES} --atomic --timeout 3m -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME} ${INPUT_BASE_CHART}"
 eval $helm_upgrade_cmd
-#helm upgrade --install "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}" -f "${INPUT_VALUES_FILE}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}"
-#helm upgrade --install ${INPUT_VALUES_FILE} --set "${INPUT_ADDITIONAL_VALUES}" --atomic --timeout 3m -n "${INPUT_NAMESPACE}" "${INPUT_RELEASE_NAME}" "${INPUT_BASE_CHART}"
 
-#helm status "${INPUT_RELEASE_NAME}"
+helm status "${INPUT_RELEASE_NAME}"
 echo "✅ Helm upgrade complete"
