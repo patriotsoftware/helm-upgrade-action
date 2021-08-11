df15579269ae9d78172b348e7fb62f0dd92cd7a0
Steve Yackey <syackey@patriotsoftware.com>
2021-08-11 10:15:00 -0400

    ShowProblems testing

--- action.sh
+++ action.sh
@@ -2,7 +2,13 @@
 
 set -eo pipefail
 
-
+show_problems() {
+    sleep 10s
+    helm status -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME}
+    echo -e "\n \n"
+    kubectl describe deploy -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME}
+    kubectl logs -n ${INPUT_NAMESPACE} deploy/${INPUT_RELEASE_NAME}
+}
 export HELM_EXPERIMENTAL_OCI=1
 
 
@@ -22,7 +28,8 @@ echo "- Run the deployment again, but check the kube-dashboard while it's deploy
 
 helm_upgrade_cmd="helm upgrade --install ${INPUT_RELEASE_NAME} ${INPUT_BASE_CHART} ${INPUT_ADDITIONAL_ARGS} ${INPUT_VALUES_FILE} --set ${INPUT_ADDITIONAL_VALUES} -n ${INPUT_NAMESPACE}"
 echo $helm_upgrade_cmd
+show_problems &
 eval $helm_upgrade_cmd
-
+kill %1 # kill the show problems so it doesn't hang
 helm status "${INPUT_RELEASE_NAME}" -n "${INPUT_NAMESPACE}"
 echo "✅ Helm upgrade complete"
