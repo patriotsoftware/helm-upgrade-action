28b76f96487dd7ac932c0e79a74d97c709811655
Steve Yackey <syackey@patriotsoftware.com>
2021-08-12 08:44:24 -0400

    Added additional cases for errors

--- action.sh
+++ action.sh
@@ -7,20 +7,56 @@ show_problems() {
     helm status -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME}
     echo -e "\n \n"
     
-    echo -e "\n Deployment Description: \n"
-    kubectl describe deploy -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME}
-    RS_NAME=`kubectl describe deployment -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME} |grep "^NewReplicaSet"|awk '{print $2}'`; echo $RS_NAME
-    echo -e "\n ReplicaSet Description: \n"
-    kubectl describe rs -n ${INPUT_NAMESPACE} $RS_NAME
-
-    POD_HASH_LABEL=$(kubectl get rs -n ${INPUT_NAMESPACE} $RS_NAME -o jsonpath="{.metadata.labels.pod-template-hash}")
-    POD_NAMES=$(kubectl get pods -n ${INPUT_NAMESPACE} -l pod-template-hash=$POD_HASH_LABEL --show-labels | tail -n +2 | awk '{print $1}')
+    echo -e "\nDeployment Description: \n"
+    deploy_description=$(kubectl describe deploy -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME})
+    echo $deploy_description
     
-    echo -e "\n Pod Logs: \n"
-    kubectl logs -n ${INPUT_NAMESPACE} deploy/${INPUT_RELEASE_NAME}
+    echo -e "\nReplicaSet Description: \n"
+    replicaset_name=$(kubectl describe deployment -n ${INPUT_NAMESPACE} ${INPUT_RELEASE_NAME} | grep "^NewReplicaSet"| awk '{print $2}')
+    replicaset_description=$(kubectl describe rs -n ${INPUT_NAMESPACE} $replicaset_name)
+    echo $replicaset_description
+    
+    echo -e "\nPod Description: \n"
+    pod_hash_label=$(kubectl get rs -n ${INPUT_NAMESPACE} $replicaset_name -o jsonpath="{.metadata.labels.pod-template-hash}")
+    pod_names=$(kubectl get pods -n ${INPUT_NAMESPACE} -l pod-template-hash=$pod_hash_label --show-labels | tail -n +2 | awk '{print $1}')
+    pod_descriptions=$(echo $pod_names | xargs kubectl describe pod -n ${INPUT_NAMESPACE})
+    echo pod_descriptions
 
-    echo -e "\n Pod Description: \n"
-    echo $POD_NAMES | xargs kubectl describe pod -n ${INPUT_NAMESPACE} {}
+    echo -e "\nPod Logs: \n"
+    pod_logs=$(echo $pod_names | xargs kubectl logs -n ${INPUT_NAMESPACE})
+    echo pod_logs
+
+    # TODO: Add Green check, red X, save outputs to variables to do quick check after printing. Check for: Startup error, Image error, No node available
+    echo "There are a variety of reasons a deployment could fail. Search the GitHub Action logs for the following headers to jump to a specific part:"
+    echo " Deployment Description"
+    echo " ReplicaSet Description"
+    echo " Pod Description"
+    echo " Pod Logs"
+    echo -e "\n\nSearching common causes. Findings will be shown below. If none are shown, take a look through each of the previous sections."
+
+    full_logs=$(echo -e "$deploy_description $replicaset_description $pod_descriptions $pod_logs")
+    
+    if [[ "$full_logs" == *"CrashLoopBackOff"* ]]; then
+        echo "CrashLoopBackoff found. This occurs when either a pod crashes during startup, or a health check probe continually fails. Check pod logs above."
+    fi
+
+    if [[ "$full_logs" == *"probe errored"* ]] || [[ "$full_logs" == *"probe failed"* ]]; then
+        echo "One or more probes (startup, liveness, readiness) has failed. Check the pod description above."
+    fi
+
+    if [[ "$full_logs" == *"ImagePullBackOff"* ]]; then
+        echo "ImagePullBackOff found. This occurs when the container image cannot be pulled. Check that the image exists and that the node has access to that image."
+    fi
+    
+    
+    if [[ "$full_logs" == *"FailedScheduling"* ]]; then
+        echo "FailedScheduling found. Check the 'Events' section of the pod description above."
+    fi
+
+    if [[ "$full_logs" == *"FailedCreate"* ]]; then
+        echo "FailedCreate found. Check the 'Events' section of the pod description above."
+    fi
+    
     
 }
 export HELM_EXPERIMENTAL_OCI=1
